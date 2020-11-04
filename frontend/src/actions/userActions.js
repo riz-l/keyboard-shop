89ddd380ec9fdfaf786a868993b069610f6d9475
Riz <joshua.riz.layton@gmail.com>
2020-11-04 13:12:28 +0000

    MASTER: Corrected imports

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,6 +8,8 @@ import {
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
   USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
