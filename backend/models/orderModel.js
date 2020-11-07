a2b99ccad5ff312546a3040da3916cbacb37c9a5
Riz <joshua.riz.layton@gmail.com>
2020-11-07 23:53:17 +0000

    MASTER: Added orderActionos, orderConstants and orderReducers.

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -10,7 +10,7 @@ const orderSchema = mongoose.Schema(
     orderItems: [
       {
         name: { type: String, required: true },
-        qty: { type: Number, required: true },
+        quantity: { type: Number, required: true },
         image: { type: String, required: true },
         price: { type: Number, required: true },
         product: {
@@ -29,7 +29,7 @@ const orderSchema = mongoose.Schema(
         type: String,
         required: true,
       },
-      postalCode: {
+      postCode: {
         type: String,
         required: true,
       },
