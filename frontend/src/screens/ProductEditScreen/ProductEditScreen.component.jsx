da5be0f8554a84d8a7453048d198d71e480c075e
Riz <joshua.riz.layton@gmail.com>
2020-11-11 00:21:24 +0000

    MASTER: Added file upload functionality to ProductEditScreen

--- frontend/src/screens/ProductEditScreen/ProductEditScreen.component.jsx
+++ frontend/src/screens/ProductEditScreen/ProductEditScreen.component.jsx
@@ -1,4 +1,5 @@
 // Import: Dependencies
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
@@ -16,7 +17,7 @@ import { PRODUCT_UPDATE_RESET } from "../../constants/productConstants";
 export default function ProductEditScreen({ match, history }) {
   const productId = match.params.id;
 
-  // State = name, price, image, branch, category, countInStock, description
+  // State = name, price, image, branch, category, countInStock, description, uploading
   const [name, setName] = useState("");
   const [price, setPrice] = useState(0);
   const [image, setImage] = useState("");
@@ -24,6 +25,7 @@ export default function ProductEditScreen({ match, history }) {
   const [category, setCategory] = useState("");
   const [countInStock, setCountInStock] = useState(0);
   const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -55,6 +57,29 @@ export default function ProductEditScreen({ match, history }) {
       }
     }
   }, [dispatch, history, productId, product, successUpdate]);
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.log(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -118,6 +143,14 @@ export default function ProductEditScreen({ match, history }) {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+
+              <Form.File
+                id="image-file"
+                label="Choose File"
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
 
             <Form.Group controlId="brand">
